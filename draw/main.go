92b1bd1b05e3c6f5f746300a441236ac6a2f5750
GoldenAmor <wfeng0221@gmail.com>
2023-04-28 23:29:54 +0800

    实现绘制元素标号功能

--- draw/main.go
+++ draw/main.go
@@ -2,13 +2,23 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
 	"fmt"
 	"gocv.io/x/gocv"
 	"image"
 	"image/color"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
+
+var url string
+
+func init() {
+	flag.StringVar(&url, "method", "", "指定调用同步/异步接口")
+	flag.Parse()
+	url = "http://localhost:6060/template-match-" + url
+}
 
 type Response struct {
 	Data []image.Rectangle `json:"data"`
@@ -17,7 +27,7 @@ type Response struct {
 }
 
 func main() {
-	resp, err := http.Get("http://localhost:6060/template-match")
+	resp, err := http.Get(url)
 	if err != nil {
 		panic(err)
 	}
@@ -34,10 +44,16 @@ func main() {
 		panic(err)
 	}
 	srcImage := gocv.IMRead("./images/src1.jpg", gocv.IMReadColor)
+	var point image.Point
 	for i := 0; i < len(data.Data); i++ {
-		gocv.Rectangle(&srcImage, data.Data[i], color.RGBA{R: 255}, 2)
+		point = image.Point{
+			X: ((data.Data[i].Max.X - data.Data[i].Min.X) / 2) + data.Data[i].Min.X,
+			Y: ((data.Data[i].Max.Y - data.Data[i].Min.Y) / 2) + data.Data[i].Min.Y}
+		gocv.Circle(&srcImage, point, 50, color.RGBA{255, 255, 255, 0}, 2)
+		gocv.PutText(&srcImage, strconv.Itoa(i+1), point, gocv.FontHersheyPlain, 4.0, color.RGBA{255, 255, 255, 0}, 2)
 	}
 	window := gocv.NewWindow("match-result")
 	window.IMShow(srcImage)
-	//fmt.Println(data.Data, data.Msg)
+	gocv.WaitKey(0)
+	fmt.Println(data.Data, data.Msg)
 }
